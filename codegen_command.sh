ed660bdbd0fd706d99112e247c542bf272817047
Anders Munk <andersmunk0412@gmail.com>
2017-04-28 15:42:16 +0200

    Updated command to build complete client

--- codegen_command.sh
+++ codegen_command.sh
@@ -1,3 +1,1 @@
-java -Dmodels -DsupportingFiles -jar swagger-codegen-cli-2.2.1.jar generate -i https://api.reepay.com/swagger.json -l csharp -o ./code -c config.json 
-
-#java -jar swagger-codegen generate -i https://api.reepay.com/swagger.json -l csharp -c config.json
+java -jar swagger-codegen-cli-2.2.1.jar generate -i https://api.reepay.com/swagger.json -l csharp -o ./src -c config.json 
